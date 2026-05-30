fcaa581d662e52d840bb2619b99971a1edc6286f
Harsha Hegde <harshahegde10s@gmail.com>
2026-05-30 20:55:11 +0530

    Update: New Resume added in download section

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 const Download = () => {
   return (
     <div className="ml-5 mt-4 relative bottom-6 md:ml-20 animate-bounce">
-      <a href="/HarshaNewResume.pdf" download="Harsha_Resume">
+      <a href="/Harsha_Updated_Resume_2026.pdf" download="Harsha_Resume">
         <Button>Download Resume</Button>
       </a>
     </div>
